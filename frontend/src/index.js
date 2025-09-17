2776352f3b7b2b223ba19ac19a486fad4dd0c641
Timothy Zucker <155255765+zuck30@users.noreply.github.com>
2025-09-17 14:03:14 +0300

    modified: index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
+import ErrorBoundary from './components/ErrorBoundary';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <ErrorBoundary>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </ErrorBoundary>
   </React.StrictMode>
 );
